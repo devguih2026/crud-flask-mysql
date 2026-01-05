f5315ab7ca022981686148ac13941c244714a840
Guilherme Fonseca <guilhermeflor18102002@gmail.com>
2026-01-05 16:40:37 -0300

    CRUD pronto e validação de usuário

--- Sistema de autenticação/controllers.py
+++ Sistema de autenticação/controllers.py
@@ -1,5 +1,5 @@
 from flask import Blueprint, request, jsonify
-from crud import Cadastrar
+from crud import Cadastrar, MostrarUsuarios, RemoverUsuario, AtualizarUsuario
 
 projeto_bp = Blueprint("projeto_bp", __name__)
 
@@ -7,10 +7,41 @@ projeto_bp = Blueprint("projeto_bp", __name__)
 def NovoUsario():
     dados = request.json
 
+    if not dados:
+        return jsonify({"erro": "JSON não enviado"}), 400 # valida se o JSON foi enviado
+    
+    if "email" not in dados or "senha" not in dados or "nome" not in dados:
+        return jsonify({"erro": "Campos obrigatórios ausentes"}), 400 # valida se email, senha ou nome existem
+
     nome = dados["nome"]
     email = dados["email"]
     senha = dados["senha"]
 
+    if not nome or not email or not senha:
+        return jsonify({"erro": "Campos não podem estar vazios"}), 400 # valida se nome, email ou senha foram preenchidos
+    
+    if not isinstance(nome, str) or not isinstance(email, str) or not isinstance(senha, str):
+        return jsonify({"erro": "Tipo de dado inválido"}), 400 # valida se os tipos de dados estão corretos
+
     chamar = Cadastrar(nome, email, senha)
 
     return jsonify(chamar), 201
+
+@projeto_bp.route("/instagram", methods=["GET"])
+def VerUsuarios():
+    chamar = MostrarUsuarios()
+    return jsonify(chamar), 200
+
+@projeto_bp.route("/instagram/usuario/<id>", methods=["PUT"])
+def AtualizarDados(id):
+    dados = request.json
+    nome = dados["nome"]
+    chamar = AtualizarUsuario(nome, id)
+    return jsonify(chamar)
+
+@projeto_bp.route("/instagram/usuario/<id>", methods=["DELETE"])
+def ApagarUsuario(id):
+    chamar = RemoverUsuario(id) 
+    return jsonify(chamar)
+
+
--- Sistema de autenticação/crud.py
+++ Sistema de autenticação/crud.py
@@ -9,3 +9,32 @@ def Cadastrar(nome, email, senha):
     cursor.close()
     conexao.close()
     return "Usuário cadastrado"
+
+def MostrarUsuarios():
+    conexao = Conectar()
+    cursor = conexao.cursor(dictionary=True)
+    cursor.execute("SELECT * FROM usuario")
+    resultado = cursor.fetchall()
+    cursor.close()
+    conexao.close()
+    return resultado
+
+def AtualizarUsuario(nome, id):
+    conexao = Conectar()
+    cursor = conexao.cursor()
+    query = "UPDATE usuario SET nome = %s WHERE id = %s"
+    cursor.execute(query, (nome, id))
+    conexao.commit()
+    cursor.close()
+    conexao.close()
+    return "Dados atualizados"
+
+def RemoverUsuario(id):
+    conexao = Conectar()
+    cursor = conexao.cursor()
+    query = "DELETE FROM usuario WHERE id = %s"
+    cursor.execute(query, (id, ))
+    conexao.commit()
+    cursor.close()
+    conexao.close()
+    return "Usuário removido"
