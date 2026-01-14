fa319678cbfc8dd72c47b84604db06b6910e6ebd
Guilherme Fonseca <guilhermeflor18102002@gmail.com>
2026-01-14 15:52:24 -0300

    CRUD dos posts com JOIN

--- API de blogs/app.py
+++ API de blogs/app.py
@@ -1,9 +1,11 @@
 from flask import Flask
-from Controllers.controller_usuarios import projeto_bp
+from Controllers.controller_usuarios import usuarios_bp
+from Controllers.controller_posts import posts_bp
 
 app = Flask(__name__)
 
-app.register_blueprint(projeto_bp)
+app.register_blueprint(usuarios_bp)
+app.register_blueprint(posts_bp)
 
 if __name__ == "__main__":
     app.run(debug=True)
--- API de blogs/crud/crud_posts.py
+++ API de blogs/crud/crud_posts.py
@@ -1,14 +1,48 @@
 from db import Conectar
 
-def NovoPost():
-    pass
+def NovoPost(titulo, conteudo, id_usuario):
+    conexao = Conectar()
+    cursor = conexao.cursor()
+    query = "INSERT INTO posts (titulo, conteudo, id_usuario) VALUES (%s, %s, %s)"
+    cursor.execute(query, (titulo, conteudo, id_usuario))
+    conexao.commit()
+    cursor.close()
+    conexao.close()
+    return "Post criado"
 
 def MostrarPosts():
-    pass
+    conexao = Conectar()
+    cursor = conexao.cursor(dictionary=True)
+    cursor.execute(""" SELECT 
+    posts.id_posts,
+    posts.titulo,            
+    posts.conteudo,               
+    usuario.nome AS usuario               
+    FROM posts               
+    JOIN usuario               
+    ON posts.id_usuario = usuario.id_usuario """)
+    resultado = cursor.fetchall()
+    cursor.close()
+    conexao.close()
+    return resultado
 
-def AtualizarPosts():
-    pass
+def AtualizarPosts(titulo, id):
+    conexao = Conectar()
+    cursor = conexao.cursor()
+    query = "UPDATE posts SET titulo = %s WHERE id_posts = %s"
+    cursor.execute(query, (titulo, id))
+    conexao.commit()
+    cursor.close()
+    conexao.close()
+    return "Titulo alterado"
 
-def ApagarPosts():
-    pass
+def ApagarPosts(id):
+    conexao = Conectar()
+    cursor = conexao.cursor()
+    query = "DELETE FROM posts WHERE id_posts = %s"
+    cursor.execute(query, (id, ))
+    conexao.commit()
+    cursor.close()
+    conexao.close()
+    return "post removido"
 
